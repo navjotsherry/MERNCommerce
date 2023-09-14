77cac74382c3369e465a0f2ae76b088f3c3a4f34
Navjot Sherry <85787077+navjotsherry@users.noreply.github.com>
2023-09-14 12:50:07 -0400

    Added database support

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import router from './routes/productRouters.js'
 
+
 const app = express()
 app.use('/api/v1',router)
 
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import app from "./app.js";
 import dotenv from 'dotenv'
+import DBConnection from "./config/database.js";
 
 dotenv.config({path:"./config/config.env"})
 
 const PORT = process.env.PORT
+console.log(process.env.DB_URI)
+
+DBConnection()
 
 
 app.listen(PORT, ()=>{
