d547b492155ee9925ee11643a5c4f29ad9ace543
Navjot Sherry <85787077+navjotsherry@users.noreply.github.com>
2023-09-15 13:03:14 -0400

    Added Products schema

---
+++ backend/config/db/productSchema.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type : String,
+        required:[true,"Please enter a valid name"]
+    },
+    description:{
+        type: String,
+        required:[true,"Please enter a valid description"]
+    },
+    price:{
+        type: Number,
+        required:[true,"Please enter a valid price."],
+        maxlength: [6,"Price should be lesser than 6 digits."]
+    },
+    images:[
+        {
+            publicId:{
+            type:String,
+            required:true
+            },
+            url:{
+                type:String,
+                required:true
+            }
+            }
+    ],
+    rating:{
+        type:Number,
+        default:0
+    },
+    category:{
+        type:String,
+        required:[true,"Please enter product category."]
+    },
+    Stock:{
+        type:Number,
+        required:[true,"Please enter product stock"],
+        maxlength:[4,"Stock cannot Exceed 4 characters"],
+        default:1
+    },
+    numOfReviews:{
+        type:Number,
+        default:0
+    },
+    reviews:[
+        {
+            name :{
+                type:String,
+                required:true
+            }, 
+            rating :{
+                type:String,
+                required:true
+            },
+            comment :{
+                type:String
+            },
+
+        }
+    ],
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+    
+})
+
+
+export default mongoose.model('Product',productSchema)
