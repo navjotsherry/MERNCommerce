81fd00246b5d8ca87a7a279aa68e265262c0a3df
Navjot Sherry <85787077+navjotsherry@users.noreply.github.com>
2023-09-19 12:31:52 -0400

    Setting up for manual error handling

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -3,79 +3,72 @@ import product from "../config/db/productSchema.js"
 
 // Create New product 
 export const createNewProduct = async (req, res)=>{
-    const {name, description , price,images,category} = req.body
-    // console.log(req.body)
-    try {
-        const productRes = await product.create({name,description,price,images,category})
+    
+        const productRes = await product.create(req.body)
         res.status(200).json({
             success: true,
             productRes
         })
-    } catch (err) {
-        console.log(err.message)
-    }
+    
 }
 
 //Get All the products
 export const getAllProducts = async (req,res)=>{
-    try {
+   
         const allProducts = await product.find()
         res.status(200).json({
             success:true,
             allProducts
         })
-    } catch (error) {
-     console.log(error.message)   
-    }
+    
 }
 
 //Get Single Product's details
 export const getProductDetails = async (req,res) => {
-    try {
-        const productDetail = await product.findById(req.params.id)
-        res.status(200).json({
-            success:true,
-            productDetail
-        })
-    } catch (error) {
-        res.status(400).json({
+    const productDetail = await product.findById(req.params.id)
+    if(!productDetail){
+        return res.status(400).json({
             success:false,
-            error
+            message:"Product not found"
         })
     }
+    res.status(200).json({
+        success:true,
+        productDetail
+    })
+   
 }
 
 // Update the product 
 export const updateProduct= async (req,res)=>{
-    try {
-        let savedProduct = await product.findById(req.params.id)
-        savedProduct.name= req.body.name
-        savedProduct.description = req.body.description
-        savedProduct.price = req.body.price
-        savedProduct.images = req.body.images
-        savedProduct.category = req.body.category
-        savedProduct.save()
-        res.status(200).json({
-            success:true,
-            savedProduct
+    let savedProduct = await product.findById(req.params.id)
+    if(!savedProduct){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
         })
-    } catch (error) {
-        res.status(400).json({success:false, error})
     }
+    savedProduct = await product.findByIdAndUpdate(req.params.id,req.body,{new:true,
+    runValidators:true,useFindAndModify:false})
+     
+    res.status(200).json({
+        success:true,
+        savedProduct
+       })
 }
 
 //Delete Product
 export const deleteProduct = async (req,res) =>{
-    try {
-        const delProduct = await product.findByIdAndDelete(req.params.id)
+        const delProduct = await product.findById(req.params.id)
+        if(!delProduct){
+            return res.status(400).json({
+                success:false,
+                message:"No such product found"
+            })
+        }
+        await delProduct.remove()
         res.status(200).json({
             success:true,
-            delProduct
+            message:"Product deleted successfully"
         })
-    } catch (error) {
-        res.status(400).json({
-            success:false,
-            error
-        })
-    }
 }
