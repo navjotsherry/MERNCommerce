97547300de75bd2e99e5c58c0d19597fda41a93e
Navjot Sherry <85787077+navjotsherry@users.noreply.github.com>
2023-11-10 09:39:02 -0500

    Added Material UI stepper

--- frontend/src/components/Shipping/Shipping.js
+++ frontend/src/components/Shipping/Shipping.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Country, State } from 'country-state-city'
+import ShippingStepper from './ShippingStepper'
 
 const Shipping = () => {
     // Initialize state variables for user information, country, and state.
@@ -15,30 +16,33 @@ const Shipping = () => {
     // Function to handle the form submission.
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(userInfo)
+        console.log({...userInfo,country,state})
     }
 
     return (
-        <div className='flex items-center justify-center min-h-[86vh]'>
-            <div className="flex flex-col items-center rounded-md border-4 min-h-[80%] border-primary">
-                <form type="submit" onSubmit={handleSubmit} className='flex flex-col items-center justify-center'>
-                    {/* Input fields for address, city, and phone. */}
-                    <div className="flex px-4 py-2 items-center justify-center border mx-8 border-black rounded-md mt-8"><input type="text" name='address' onChange={handleUserInfo} value={userInfo.address} className='mx-2 bg-transparent outline-none' placeholder='Address' /></div>
-                    <div className="flex px-4 py-2 items-center justify-center border mx-8 border-black rounded-md mt-8"><input type="text" name='city' onChange={handleUserInfo} value={userInfo.city} className='mx-2 bg-transparent outline-none' placeholder 'City' /></div>
-                    <div className="flex px-4 py-2 items-center justify-center border mx-8 border-black rounded-md mt-8"><input type="tel" name='phone' onChange={handleUserInfo} value={userInfo.phone} className='mx-2 bg-transparent outline-none' placeholder='Phone' /></div>
-                    {/* Dropdowns for selecting country and state. */}
-                    <div className="flex px-4 py-2 items-center justify-center border mx-8 border-black rounded-md mt-8"><select onChange={(e) => setCountry(e.target.value)} type="text" className='w-52 bg-transparent outline-none'>
-                        {Country.getAllCountries().map(element => <option value={element.isoCode} key={element.isoCode}>{element.name}</option>)}</select>
-                    </div>
-                    {/* Display the state dropdown when a country is selected. */}
-                    {country && <div className="flex px-4 py-2 items-center w-60 justify-center border mx-8 border-black rounded-md mt-8"><select onChange={(e) => setState(e.target.value)} value={country} type="text" className='mx-2 bg-transparent w-56 outline-none'>
-                        {State.getStatesOfCountry(country).map(element => <option onClick={() => setState(element.isoCode)} value={element.isoCode} key={element.isoCode}>{element.name}</option>)}</select>
-                    </div>}
-                    {/* Submit button to save user information. */}
-                    <button type="submit" className='my-8 px-4 py-2 bg-primary text-2xl rounded-md w-[80%] hover:bg-black hover:text-primary duration-300'>Save</button>
-                </form>
+        <>
+            <ShippingStepper activeStep={0}/>
+            <div className='flex items-center justify-center min-h-[86vh]'>
+                <div className="flex flex-col items-center rounded-md border-4 min-h-[80%] border-primary">
+                    <form type="submit" onSubmit={handleSubmit} className='flex flex-col items-center justify-center'>
+                        {/* Input fields for address, city, and phone. */}
+                        <div className="flex px-4 py-2 items-center justify-center border mx-8 border-black rounded-md mt-8"><input type="text" name='address' onChange={handleUserInfo} value={userInfo.address} className='mx-2 bg-transparent outline-none' placeholder='Address' /></div>
+                        <div className="flex px-4 py-2 items-center justify-center border mx-8 border-black rounded-md mt-8"><input type="text" name='city' onChange={handleUserInfo} value={userInfo.city} className='mx-2 bg-transparent outline-none' placeholder='City' /></div>
+                        <div className="flex px-4 py-2 items-center justify-center border mx-8 border-black rounded-md mt-8"><input type="tel" name='phone' onChange={handleUserInfo} value={userInfo.phone} className='mx-2 bg-transparent outline-none' placeholder='Phone' /></div>
+                        {/* Dropdowns for selecting country and state. */}
+                        <div className="flex px-4 py-2 items-center justify-center border mx-8 border-black rounded-md mt-8"><select onChange={(e) => setCountry(e.target.value)} type="text" className='w-52 bg-transparent outline-none'>
+                            {Country.getAllCountries().map(element => <option value={element.isoCode} key={element.isoCode}>{element.name}</option>)}</select>
+                        </div>
+                        {/* Display the state dropdown when a country is selected. */}
+                        {country && <div className="flex px-4 py-2 items-center w-60 justify-center border mx-8 border-black rounded-md mt-8"><select onChange={(e) => setState(e.target.value)} value={country} type="text" className='mx-2 bg-transparent w-56 outline-none'>
+                            {State.getStatesOfCountry(country).map(element => <option onClick={() => setState(element.isoCode)} value={element.isoCode} key={element.isoCode}>{element.name}</option>)}</select>
+                        </div>}
+                        {/* Submit button to save user information. */}
+                        <button type="submit" disabled={state ? false : true} className='my-8 px-4 py-2 bg-primary text-2xl rounded-md w-[80%] hover:bg-black disabled:hover:text-black hover:text-primary disabled:bg-yellow-900 duration-300'>Save</button>
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
---
+++ frontend/src/components/Shipping/ShippingStepper.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Stepper from '@mui/material/Stepper'
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import { Typography } from '@mui/material';
+import {LiaShippingFastSolid} from 'react-icons/lia'
+import {GiConfirmed} from 'react-icons/gi'
+import {BiSolidBank} from 'react-icons/bi'
+
+
+const steps = [
+    {
+        label : <Typography>Shipping Details</Typography>,
+        icon : <LiaShippingFastSolid/>
+    },
+    {
+        label : <Typography>Confirm Order</Typography>,
+        icon : <GiConfirmed/>
+    },
+    {
+        label : <Typography>Payment</Typography>,
+        icon : <BiSolidBank/>
+    }
+]
+
+const stepStyles = {
+    boxSizing : "border-box"
+}
+
+const ShippingStepper = ({activeStep}) => {
+  return (
+    <>
+        <div className="hidden md:block mt-8">
+            <Stepper alternativeLabel activeStep={activeStep} style={stepStyles} >
+                {steps.map((item,index)=>{
+                    return <Step key={item.label}
+                     active={activeStep===index ? true : false}
+                     completed={activeStep >= index ? true : false}
+                     ><StepLabel icon={item.icon}>{item.label}</StepLabel></Step>
+                })}
+            </Stepper>
+        </div>
+    </>
+    
+  )
+}
+
+export default ShippingStepper
