6e1499fb597ceeaa2bd48b329805a7944103e942
Navjot Sherry <85787077+navjotsherry@users.noreply.github.com>
2023-11-24 22:15:38 -0500

    Code comments added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,25 +1,34 @@
-import {GiMouse} from 'react-icons/gi'
-import MetaData from '../utils/MetaData'
-import FeaturedProducts from './FeaturedProducts'
+// Importing necessary modules and components
+import { GiMouse } from 'react-icons/gi';
+import MetaData from '../utils/MetaData';
+import FeaturedProducts from './FeaturedProducts';
 
+// Functional component for the Home page
 const Home = () => {
-  
-  return (
-    <>
-    <MetaData title="Home"/>
-    <div className='h-[88vh] flex flex-col justify-center items-center clip-your-needful-style bg-gradient-to-r from-primary to-yellow-300'>
-      <h1 className='text-3xl'>Welcome to E-Commerce</h1>
-      <h1 className='text-2xl mt-4'>Get Amazing products Below</h1>
-      <h1  className='text-xl border border-1 rounded-md flex items-center mt-6 p-2 border-black hover:bg-black hover:text-primary hover:cursor-pointer transition-all duration-300'><GiMouse/> Scroll</h1>
-    </div>
-    <div className='text-4xl text-center underline m-14'>Featured Products</div>
-      <FeaturedProducts/>
-    </>
-  )
+    // Rendering the component
+    return (
+        <>
+            {/* Metadata component for page title */}
+            <MetaData title="Home" />
+            
+            {/* Main content section */}
+            <div className='h-[88vh] flex flex-col justify-center items-center clip-your-needful-style bg-gradient-to-r from-primary to-yellow-300'>
+                {/* Welcome message */}
+                <h1 className='text-3xl'>Welcome to E-Commerce</h1>
+                <h1 className='text-2xl mt-4'>Get Amazing products Below</h1>
+                {/* Scroll indicator */}
+                <h1 className='text-xl border border-1 rounded-md flex items-center mt-6 p-2 border-black hover:bg-black hover:text-primary hover:cursor-pointer transition-all duration-300'>
+                    <GiMouse /> Scroll
+                </h1>
+            </div>
+
+            {/* Featured Products section */}
+            <div className='text-4xl text-center underline m-14'>Featured Products</div>
+            {/* Featured Products component */}
+            <FeaturedProducts />
+        </>
+    );
 }
 
-export default Home
-
-
-
-
+// Exporting the Home component
+export default Home;
